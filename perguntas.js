e3dff8a86b084daa23a45703aa3c76878f62782d
Viviane <viviane.luggeri.oliveira@escola.pr.gov.br>
2025-05-20 14:48:16 +0000

    aula-2

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Disney',
+    'O que é a plataforma Disney+?',
+    'Disney+ é uma plataforma de filmes e'
+)
+
+criaCartao(
+    'Filme',
+    'Qual é o filme mais conhecido do gênero terror?',
+    'O filme de terror mais conhecido é Annabelle '
+)
+
+criaCartao(
+    'Montanha',
+    'Qual a montanha mais alta do mundo?',
+    'A montanha mais alta do mundo é o Monte Everest'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gato em Inglês?',
+    'Gato em ingles é CAT (kati)'
+)
+
+
+criaCartao(
+    'Planetas',
+    'Qual o maior planeta do sistema solar?',
+    'Júpter'
+)
+
+criaCartao(
+    'Olimpíadas',
+    'O que representam os cinco anéis olímpicos?',
+    'As partes do mundo unidas pelo Olimpismo'
+)
+
+criaCartao(
+    'Chuva',
+    'Que cidade brasileira é conhecida por chover todos os dias quase à mesma hora?',
+    'Belém é a cidade que chove quase rodo dia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?',
+    'A cientista Marie Curie venceu o Prêmio Nobel'
+)
